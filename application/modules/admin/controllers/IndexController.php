3dd05e55c6e522a26739cbcbe5ff20ab87a1fd04
Fabrice MARSAUD <marsaud.fabrice@neuf.fr>
2012-10-14 23:01:57 +0200

    a clean base for production

--- application/modules/admin/controllers/IndexController.php
+++ application/modules/admin/controllers/IndexController.php
@@ -10,7 +10,7 @@ class Admin_IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        $this->_forward('menu');
+        
     }
 
     public function menuAction()
